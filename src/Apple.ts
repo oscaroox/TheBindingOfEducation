67eae01e4305b7a16cde977a61e7f3f73872e007
Sander Vispoel <sandervispoel@hotmail.com>
2016-05-16 17:18:12 +0200

    11 - global canvas selector changes
    We no longer use locally stored pointers to canvas, we instead grab it
    from the Defines file

--- src/Apple.ts
+++ src/Apple.ts
@@ -2,12 +2,12 @@ import Fruit from './Fruit';
 
 export default class Apple extends Fruit
 {
-    constructor(x: number, y: number, stage: CanvasRenderingContext2D)
+    constructor(x: number, y: number)
     {
         // static starting values
         var sprite = "apple.png",
             points = 8;
 
-        super(x, y, sprite, stage, points);
+        super(x, y, sprite, points);
     }
 }
--- src/Banana.ts
+++ src/Banana.ts
@@ -2,12 +2,12 @@ import Fruit from './Fruit';
 
 export default class Banana extends Fruit
 {
-    constructor(x: number, y: number, stage: CanvasRenderingContext2D)
+    constructor(x: number, y: number)
     {
         // static starting values
         var sprite = "banana.png",
             points = 5;
 
-        super(x, y, sprite, stage, points);
+        super(x, y, sprite, points);
     }
 }
