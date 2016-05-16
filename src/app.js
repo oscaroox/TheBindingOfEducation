20b90d28837a4f9b47dde42bf9d8e130dd59e42a
Sander Vispoel <sandervispoel@hotmail.com>
2016-05-16 17:21:12 +0200

    13 - file cleanup

--- src/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var GameScene_1 = require('./GameScene');
-// START
-// initialize and start game scene
-var gameScene = new GameScene_1["default"]();
-//# sourceMappingURL=app.js.map
