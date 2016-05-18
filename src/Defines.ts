b4278b8f57020e8ac2d60a2c447734c8f502c78f
Sander Vispoel <sandervispoel@hotmail.com>
2016-05-19 00:30:55 +0200

    18 - small changes to canvas, defines and new bg sizes

--- src/Defines.ts
+++ src/Defines.ts
@@ -1,14 +1,31 @@
+export var canvas = <HTMLCanvasElement> document.getElementById('canvas');
+export var ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+
 export enum Animation_State {
-    Idle,
-    Run,
-    Jump
+    ANIM_IDLE,
+    ANIM_RUN,
+    ANIM_JUMP
 }
 
 export enum Input_State {
-    None,
-    Touch,
-    Click
+    INPUT_NONE,
+    INPUT_TOUCH,
+    INPUT_CLICK
 }
 
-export var canvas = <HTMLCanvasElement> document.getElementById('canvas');
-export var ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+export enum Click_Position {
+    POS_LEFT,
+    POS_RIGHT
+}
+
+export enum Lane {
+    LANE_LEFT = 0,
+    LANE_MIDDLE,
+    LANE_RIGHT
+}
+
+export var Lane_Position = [
+    494,
+    617,
+    744
+];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import GameScene from './GameScene';
+import * as d from './Defines';
 
 // START
+// adjust canvas size to screen
+d.canvas.width = window.innerWidth;
+d.canvas.height = window.innerHeight;
+
 // initialize and start game scene
 var gameScene = new GameScene();
